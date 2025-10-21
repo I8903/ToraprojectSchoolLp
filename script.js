852f098a1ffafd34e06e041e75773823faea4c91
I8903 <maruryoyuu8998@gmail.com>
2025-10-22 04:09:23 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,5 @@
 'use strict';
 
-// 下からふわっと表示
-{
-    document.addEventListener("DOMContentLoaded", () => {
-        const target = document.querySelector(".empathy");
-        const io = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add("is-visible");
-                    io.unobserve(entry.target);
-                }
-            });
-        }, { threshold: 0.7 });
-        io.observe(target);
-    });
-}
-
-{
-    // 複数要素の下からふわっと表示
-    document.addEventListener("DOMContentLoaded", () => {
-        const targets = document.querySelectorAll(".fade-up");
-        const io = new IntersectionObserver(entries => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    entry.target.classList.add("is-visible");
-                    io.unobserve(entry.target); // 一度だけ実行
-                }
-            });
-        }, { threshold: 0.7 });
-
-        targets.forEach(target => io.observe(target));
-    });
-
-}
 
 {
     document.addEventListener("DOMContentLoaded", () => {
@@ -44,7 +11,7 @@
                     io.unobserve(entry.target);
                 }
             });
-        }, { threshold: 0.7 });
+        }, { threshold: 0.6 });
 
         targets.forEach(target => io.observe(target));
     });
