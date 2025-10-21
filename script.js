b089a627337bef17fa2e3849114b6d40e0f3211b
I8903 <maruryoyuu8998@gmail.com>
2025-10-22 03:47:22 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// 下からふわっと表示
+{
+    document.addEventListener("DOMContentLoaded", () => {
+        const target = document.querySelector(".empathy");
+        const io = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("is-visible");
+                    io.unobserve(entry.target);
+                }
+            });
+        }, { threshold: 0.2 });
+        io.observe(target);
+    });
+}
+
+{
+    // 複数要素の下からふわっと表示
+    document.addEventListener("DOMContentLoaded", () => {
+        const targets = document.querySelectorAll(".fade-up");
+        const io = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("is-visible");
+                    io.unobserve(entry.target); // 一度だけ実行
+                }
+            });
+        }, { threshold: 0.2 });
+
+        targets.forEach(target => io.observe(target));
+    });
+
+}
+
+{
+    document.addEventListener("DOMContentLoaded", () => {
+        const targets = document.querySelectorAll(".fade-up, .slide-in , .slide-in-right");
+        const io = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("is-visible");
+                    io.unobserve(entry.target);
+                }
+            });
+        }, { threshold: 0.2 });
+
+        targets.forEach(target => io.observe(target));
+    });
+}
+
